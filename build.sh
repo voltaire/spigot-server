dcaaf477e07bf48566072fb3fea80edb267024c5
Jon Chen <bsd@voltaire.sh>
2020-10-03 20:31:39 +0000

    fix minecraft version tag

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ if [ -z "${DOCKER_TAG}"]
 then
     MINECRAFT_VERSION="latest"
 else
-    MINECRAFT_VERSION="${SOURCE_BRANCH}"
+    MINECRAFT_VERSION="${DOCKER_TAG}"
 fi
 
 java -jar /BuildTools.jar --rev ${MINECRAFT_VERSION}
