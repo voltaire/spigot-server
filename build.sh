559a824c0f58151c0ccee32f1987375f96f9b876
Jon Chen <bsd@voltaire.sh>
2020-10-05 04:29:43 +0000

    use build hook

--- build.sh
+++ build.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-env
-
-if [ -z "${DOCKER_TAG}"]
-then
-    MINECRAFT_VERSION="latest"
-else
-    MINECRAFT_VERSION="${DOCKER_TAG}"
-fi
-
 echo "building for ${MINECRAFT_VERSION}"
 java -jar /BuildTools.jar --rev ${MINECRAFT_VERSION}
 find . -regextype posix-egrep -regex '\./spigot-([0-9\.].*).jar' -exec mv {} /spigot.jar \;
