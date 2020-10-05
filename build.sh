d5f7e604457de6b6a1261df9c4b7b5004fe98121
Jon Chen <bsd@voltaire.sh>
2020-10-05 03:52:06 +0000

    echo version

--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ else
     MINECRAFT_VERSION="${DOCKER_TAG}"
 fi
 
+echo "building for ${MINECRAFT_VERSION}"
 java -jar /BuildTools.jar --rev ${MINECRAFT_VERSION}
 find . -regextype posix-egrep -regex '\./spigot-([0-9\.].*).jar' -exec mv {} /spigot.jar \;
