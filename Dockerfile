375f172401c235bab617f513dd292346bcef254f
Jon Chen <bsd@voltaire.sh>
2020-12-14 18:42:36 +0000

    create github release

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,2 @@
-FROM amazoncorretto:8-alpine-jre AS builder
-ARG MINECRAFT_VERSION=latest
-ADD https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar /BuildTools.jar
-RUN apk add --no-cache git ca-certificates findutils
-WORKDIR /build
-ADD build.sh /build.sh
-RUN /build.sh
-
 FROM amazoncorretto:8-alpine-jre
-COPY --from=builder /spigot.jar /spigot.jar
+ADD ./spigot.jar /spigot.jar
--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+echo "fetching buildtools"
+wget -O ./BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+
 echo "building for ${MINECRAFT_VERSION}"
-java -jar /BuildTools.jar --rev ${MINECRAFT_VERSION}
-find . -regextype posix-egrep -regex '\./spigot-([0-9\.].*).jar' -exec mv {} /spigot.jar \;
+java -jar ./BuildTools.jar --rev ${MINECRAFT_VERSION}
+
+echo "rename spigot.jar"
+find . -regextype posix-egrep -regex '\./spigot-([0-9\.].*).jar' -exec mv {} ./spigot.jar \;
